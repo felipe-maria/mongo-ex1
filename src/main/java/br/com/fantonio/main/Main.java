4e39bfeaa58be08562812b97098b8872b6cc8f85
Felipe Antonio Maria <felipe.antonio.maria@gmail.com>
2024-12-06 19:47:08 -0300

    First running version with Mongodb Not yet integrated with java

--- src/main/java/br/com/fantonio/main/Main.java
+++ src/main/java/br/com/fantonio/main/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args)  {
         // Usa o ClassLoader para obter o InputStream do arquivo
         InputStream inputStream = Main.class.getClassLoader().getResourceAsStream("ascii/fantonio.txt");
         // Verifica se o arquivo foi encontrado
